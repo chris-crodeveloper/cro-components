8c8fb756082356cec411f7735a5a3cf76da649be
Chris Davies <chris.davies2@virginmediao2.co.uk>
2025-07-23 12:46:15 +0100

    fixing issues

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,8 @@ const path = require("path");
 
 const rootDir = path.resolve(require.main.path, "../..");
 const croComponentsPath = path.resolve(rootDir, "cro-components");
+console.log("Loading stories from:", path.join(croComponentsPath, "**/*.stories.@(js|jsx|ts|tsx|mdx)"));
+
 
 module.exports = {
   stories: [
@@ -25,9 +27,19 @@ module.exports = {
     options: {}
   },
   webpackFinal: async (config) => {
+    // Add cro-components path so webpack can resolve components
     if (fs.existsSync(croComponentsPath)) {
       config.resolve.modules = config.resolve.modules || [];
       config.resolve.modules.push(croComponentsPath);
+
+      // Allow webpack to process cro-components files outside of package root
+      config.module.rules.forEach((rule) => {
+        if (rule.test && rule.test.toString().includes('tsx|ts|jsx|js')) {
+          rule.include = Array.isArray(rule.include)
+            ? [...rule.include, croComponentsPath]
+            : [rule.include, croComponentsPath];
+        }
+      });
     }
     return config;
   }
